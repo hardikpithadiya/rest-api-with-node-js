f3fd52435c5ca9f9084cf8acd3fa63b00081ade8
Hardik <nt.hardik@gmail.com>
2025-02-10 23:43:41 +0530

    added simple validations

--- controllers/feed.controller.js
+++ controllers/feed.controller.js
@@ -1,24 +1,42 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
+        _id: 1,
         title: "First post",
         desc: "Hello World !",
+        imageUrl: "images/ROK.jpg",
+        creator: {
+          name: "Hardik",
+        },
+        createdAt: new Date(),
       },
     ],
   });
 };
 
 exports.addPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.errors) {
+    return res.status(422).json({
+      message: "Invalid data",
+      errors: errors.errors,
+    });
+  }
+
   const title = req.body.title;
   const content = req.body.content;
   res.status(201).json({
-    posts: [
-      {
-        postAddedOn: Date.now(),
-        title: title,
-        desc: content,
+    post: {
+      _id: Date.now().toLocaleString(),
+      title: title,
+      desc: content,
+      creator: {
+        name: "Hardik",
       },
-    ],
+      createdAt: new Date(),
+    },
   });
 };
--- routes/feed.route.js
+++ routes/feed.route.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const feedController = require("../controllers/feed.controller");
+const { body } = require("express-validator");
 
 const router = express.Router();
 
 // GET -> /feed/posts
 router.get("/posts", feedController.getPosts);
-router.post("/posts", feedController.addPost);
+
+// POST -> /feed/posts
+router.post(
+  "/posts",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.addPost
+);
 
 module.exports = router;
