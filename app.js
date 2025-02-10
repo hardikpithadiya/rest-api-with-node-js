935cb12d48b6674a4389ff124e9e55145170470a
Hardik <nt.hardik@gmail.com>
2025-02-10 22:44:43 +0530

    Added CORS

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// Handle CORS
+app.use((req, res, next) => {
+  //For Domains
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  //For Mmethods
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+
+  //For Headers
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  next();
+});
+
 //Route initialize
 app.use("/feed", feedRoutes);
 
