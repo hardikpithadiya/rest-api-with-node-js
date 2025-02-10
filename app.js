72764508a3dd790b2754a5592e29690dc7e92103
Hardik <nt.hardik@gmail.com>
2025-02-11 00:49:52 +0530

    added post list, add and get single post

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const path = require("path");
 const feedRoutes = require("./routes/feed.route");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 // Handle CORS
 app.use((req, res, next) => {
@@ -23,4 +26,20 @@ app.use((req, res, next) => {
 //Route initialize
 app.use("/feed", feedRoutes);
 
-app.listen(8080);
+//Error Handling
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.status(error.statusCode || 500).json(error.message);
+});
+
+const connectionString =
+  "mongodb+srv://hardikpithadiya:Kssm37!2@cluster0.v11xh.mongodb.net/rest-demo?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(connectionString)
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- controllers/feed.controller.js
+++ controllers/feed.controller.js
@@ -1,42 +1,67 @@
 const { validationResult } = require("express-validator");
+const Post = require("../models/post.model");
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: 1,
-        title: "First post",
-        desc: "Hello World !",
-        imageUrl: "images/ROK.jpg",
-        creator: {
-          name: "Hardik",
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then((result) => {
+      res.status(200).json({
+        posts: result,
+      });
+    })
+    .catch((error) => {
+      error.statusCode = 500;
+      next(error);
+    });
+};
+
+exports.getSinglePost = (req, res, next) => {
+  Post.findById(req.params.postId)
+    .then((result) => {
+      if (!result) {
+        const error = new Error("Not found");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({
+        post: result,
+      });
+    })
+    .catch((error) => {
+      next(error);
+    });
 };
 
 exports.addPost = (req, res, next) => {
   const errors = validationResult(req);
-  if (errors.errors) {
-    return res.status(422).json({
-      message: "Invalid data",
-      errors: errors.errors,
-    });
+  if (errors.errors && errors.errors.length > 0) {
+    const error = new Error("Invalid data");
+    error.statusCode = 422;
+    throw error;
   }
 
   const title = req.body.title;
   const content = req.body.content;
-  res.status(201).json({
-    post: {
-      _id: Date.now().toLocaleString(),
-      title: title,
-      desc: content,
-      creator: {
-        name: "Hardik",
-      },
-      createdAt: new Date(),
+  const imageUrl = req.body.imageUrl;
+
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: imageUrl,
+    creator: {
+      name: "Hardik",
     },
   });
+  post
+    .save()
+    .then((result) => {
+      res.status(200).json({
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
