f89fa2d392c9f68236709bbb3bd7e2e411c489af
Hardik <nt.hardik@gmail.com>
2025-02-11 23:32:52 +0530

    CRUD Operations done

--- controllers/feed.controller.js
+++ controllers/feed.controller.js
@@ -1,5 +1,7 @@
 const { validationResult } = require("express-validator");
 const Post = require("../models/post.model");
+const fs = require("fs");
+const path = require("path");
 
 exports.getPosts = (req, res, next) => {
   Post.find()
@@ -70,4 +72,85 @@ exports.addPost = (req, res, next) => {
       }
       next(err);
     });
+};
+
+exports.updatePost = (req, res, next) => {
+  console.log("here");
+  const errors = validationResult(req);
+  if (errors.errors && errors.errors.length > 0) {
+    const error = new Error("Invalid data");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const postId = req.params.postId;
+  const title = req.body.title;
+  const content = req.body.content;
+  let imageUrl = req.body.image;
+  if (req.file) {
+    imageUrl = req.file.path.replace("\\", "/");
+  }
+
+  if (!imageUrl) {
+    const error = new Error("No image provided..");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Not found");
+        error.statusCode = 404;
+        throw error;
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.content = content;
+      post.imageUrl = imageUrl;
+      return post.save();
+    })
+    .then((result) => {
+      res
+        .status(200)
+        .json({ message: "Product updated successfully...", post: result });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Not found");
+        error.statusCode = 404;
+        throw error;
+      }
+      clearImage(post.imageUrl);
+      return Post.findOneAndDelete(postId);
+    })
+    .then(() => {
+      res.status(200).json({ message: "Product Deleted successfully..." });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "../images", filePath);
+  fs.unlink(filePath, (err) => {
+    console.log(err);
+  });
 };
